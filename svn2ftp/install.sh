c76b9908425b979c3ebc8c07fb980b0c98302270
Onur Yılmaz <csonuryilmaz@gmail.com>
2016-07-30 18:07:05 +0300

    Deleting old project

--- svn2ftp/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Clearing current installation if exists ..."
-sudo rm -f /usr/local/bin/svn2ftp.run 
-echo "Installing ..."
-sudo ln -s $PWD/svn2ftp.run /usr/local/bin/svn2ftp.run
-sudo chmod +x /usr/local/bin/svn2ftp.run
-echo "Installed :)"
-exit 1
