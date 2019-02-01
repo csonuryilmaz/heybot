e4a102a1337e4fefc97f2bd631b6575dabedaeff
Onur Yılmaz <csonuryilmaz@gmail.com>
2019-02-01 10:57:18 +0300

    Modifications for MacOS HighSierra. * Leading trimming for `wc` output. * Cut index is decided by checking grep version.

--- heybot/scripts/update-plist.sh
+++ heybot/scripts/update-plist.sh
@@ -22,17 +22,27 @@ if [ "$3" != $VERSION ] && [ "$3" != $BUILD ]; then
   exit -1;
 fi
 
+GREP_VERSION=`grep --version | head -1`
+if [[ $GREP_VERSION == *"BSD"* ]]; then
+  echo "[i] Using BSD version of Grep command to find patterns in file."
+  CUT_INDEX=2
+else
+  echo "[i] Using GNU version of Grep command to find patterns in file."
+  CUT_INDEX=1
+fi
+echo $GREP_VERSION
+
 PATTERN="<key>"$3"</key>"
 echo "[i] File: "$1
 echo "[*] Finding pattern "$PATTERN" ..."
-LINE=`grep -nri $PATTERN $1 | cut -d ':' -f 1`
+LINE=`grep -nri $PATTERN $1 | cut -d ':' -f $CUT_INDEX`
 echo "[.] Found line: "$LINE
 echo "[1] Creating new file with lines up to pattern ..."
 head -n $LINE $1 > $1"_tmp"
 echo "[2] Appending new pattern value as "$2" ..."
 echo -e "\t<string>"$2"</string>" >> $1"_tmp"
 echo "[3] Appending other lines after pattern value ..."
-TOTAL=`wc -l $1 | cut -d ' ' -f 1`
+TOTAL=`wc -l $1 | xargs | cut -d ' ' -f 1`
 echo "[i] Total line count of file: "$TOTAL
 tail -n $(( $TOTAL-($LINE+1) )) $1 >> $1"_tmp"
 echo "[*] Replacing old file with new one ..."
