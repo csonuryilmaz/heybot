0f4d96d625e2c186bf1fa6334de67a5d354f9594
Onur Yılmaz <csonuryilmaz@gmail.com>
2019-02-23 17:00:26 +0300

    WIP: New feature -> begin-issue-with-git is initialized!
    * Fixed java coding style and code formatting.
    * `supportedProtocols` > `SUPPORTED_PROTOCOLS` renamed.
    * Used java8 `Optinal` type for avoiding null pointer dereferencing.

--- heybot/src/operation/BeginIssueWithGit.java
+++ heybot/src/operation/BeginIssueWithGit.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.FetchCommand;
 import org.eclipse.jgit.api.Git;
@@ -35,7 +36,8 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.util.FS;
 import utilities.Properties;
 
-public class BeginIssueWithGit extends Operation {
+public class BeginIssueWithGit extends Operation
+{
 
     // mandatory
     private final static String PARAMETER_REDMINE_URL = "REDMINE_URL";
@@ -43,13 +45,13 @@ public class BeginIssueWithGit extends Operation {
 
     private final static String PARAMETER_ISSUE = "ISSUE";
     private final static String PARAMETER_GIT_REPOSITORY = "GIT_REPOSITORY";
-    private final static String PARAMETER_GIT_PROTOCOL="GIT_PROTOCOL";
-    
+    private final static String PARAMETER_GIT_PROTOCOL = "GIT_PROTOCOL";
+
     // conditional optional
-    private final static String PARAMETER_GIT_USERNAME="GIT_USERNAME";
-    private final static String PARAMETER_GIT_PASSWORD="GIT_PASSWORD";
-    private final static String PARAMETER_SSH_PRIVATE_KEY="SSH_PRIVATE_KEY";
-    
+    private final static String PARAMETER_GIT_USERNAME = "GIT_USERNAME";
+    private final static String PARAMETER_GIT_PASSWORD = "GIT_PASSWORD";
+    private final static String PARAMETER_SSH_PRIVATE_KEY = "SSH_PRIVATE_KEY";
+
     // optional
     private final static String PARAMETER_BEGAN_STATUS = "BEGAN_STATUS";
     private final static String PARAMETER_ASSIGNEE_ID = "ASSIGNEE_ID";
@@ -64,90 +66,90 @@ public class BeginIssueWithGit extends Operation {
     private final static String PARAMETER_REMOTE_PORT = "REMOTE_PORT";
 
     private RedmineManager redmineManager;
-    
-    private final static HashSet<String> supportedProtocols = new HashSet<>(Arrays.asList("http", "https","ssh"));
+
+    private final static HashSet<String> SUPPORTED_PROTOCOLS = new HashSet<>(Arrays.asList("http", "https", "ssh"));
     private CredentialsProvider credentialsProvider;
     private TransportConfigCallback transportConfigCallback;
     private String repository;
 
     public BeginIssueWithGit() {
-        super(new String[]{
-            PARAMETER_ISSUE, PARAMETER_REDMINE_TOKEN, PARAMETER_REDMINE_URL, PARAMETER_GIT_REPOSITORY, PARAMETER_GIT_PROTOCOL
-        });
+	super(new String[]{
+	    PARAMETER_ISSUE, PARAMETER_REDMINE_TOKEN, PARAMETER_REDMINE_URL, PARAMETER_GIT_REPOSITORY, PARAMETER_GIT_PROTOCOL
+	});
     }
 
     @Override
     protected void execute(Properties prop) throws Exception {
-        if (areMandatoryParametersNotEmpty(prop)) {
-            String redmineAccessToken = getParameterString(prop, PARAMETER_REDMINE_TOKEN, false);
-            String redmineUrl = getParameterString(prop, PARAMETER_REDMINE_URL, false);
-            redmineManager = RedmineManagerFactory.createWithApiKey(redmineUrl, redmineAccessToken);
+	if (areMandatoryParametersNotEmpty(prop)) {
+	    String redmineAccessToken = getParameterString(prop, PARAMETER_REDMINE_TOKEN, false);
+	    String redmineUrl = getParameterString(prop, PARAMETER_REDMINE_URL, false);
+	    redmineManager = RedmineManagerFactory.createWithApiKey(redmineUrl, redmineAccessToken);
 
-            Issue issue;
-            if ((issue = getIssue(redmineManager, getParameterInt(prop, PARAMETER_ISSUE, 0))) == null) {
-                return;
-            }
-            System.out.println("#" + issue.getId() + " - " + issue.getSubject());
-            if (!isIssueAssigneeOk(issue, prop) || !isGitCredentialsOk(prop)) {
-                return;
-            }
-            createBranch(issue, prop);
-        }
+	    Issue issue;
+	    if ((issue = getIssue(redmineManager, getParameterInt(prop, PARAMETER_ISSUE, 0))) == null) {
+		return;
+	    }
+	    System.out.println("#" + issue.getId() + " - " + issue.getSubject());
+	    if (!isIssueAssigneeOk(issue, prop) || !isGitCredentialsOk(prop)) {
+		return;
+	    }
+	    createBranch(issue, prop);
+	}
     }
-    
+
     private boolean isIssueAssigneeOk(Issue issue, Properties prop) {
-        int assigneeId = getParameterInt(prop, PARAMETER_ASSIGNEE_ID, 0);
-        System.out.println("[i] #" + issue.getId() + " is assigned to " + issue.getAssigneeName() + ".");
-        if (assigneeId > 0) {
-            System.out.println("[*] Checking issue assignee ...");
-            if (issue.getAssigneeId() != assigneeId) {
-                System.out.println("\t[e] Issue assignee failed!");
-                System.out.println("\t[i] (suggested option!) Change assignee to yourself if you're sure to begin this issue.");
-                System.out.println("\t[i] (not suggested but) You can comment " + PARAMETER_ASSIGNEE_ID + " from config file to disable assignee check.");
-                return false;
-            } else {
-                System.out.println("\t[✓] Issue assignee is ok.");
-            }
-        } else {
-            System.out.println("[i] Issue assignee check is disabled.");
-        }
-        return true;
+	int assigneeId = getParameterInt(prop, PARAMETER_ASSIGNEE_ID, 0);
+	System.out.println("[i] #" + issue.getId() + " is assigned to " + issue.getAssigneeName() + ".");
+	if (assigneeId > 0) {
+	    System.out.println("[*] Checking issue assignee ...");
+	    if (issue.getAssigneeId() != assigneeId) {
+		System.out.println("\t[e] Issue assignee failed!");
+		System.out.println("\t[i] (suggested option!) Change assignee to yourself if you're sure to begin this issue.");
+		System.out.println("\t[i] (not suggested but) You can comment " + PARAMETER_ASSIGNEE_ID + " from config file to disable assignee check.");
+		return false;
+	    } else {
+		System.out.println("\t[✓] Issue assignee is ok.");
+	    }
+	} else {
+	    System.out.println("[i] Issue assignee check is disabled.");
+	}
+	return true;
     }
 
+    @SuppressWarnings("UseSpecificCatch")
     private boolean isGitCredentialsOk(Properties prop) {
-        System.out.println("[*] Checking git credentials ...");
-        String protocol = getParameterString(prop, PARAMETER_GIT_PROTOCOL, true);
-        if (!supportedProtocols.contains(protocol))
-        {
-            System.out.println("[e] Unrecognized git protocol: " + protocol);
-            System.out.println("[i] Supported protocols: " + String.join(",", supportedProtocols));
-            return false;
-        }
-        repository = getRepository(trimRight(getParameterString(prop, PARAMETER_GIT_REPOSITORY, false), "/"), protocol);
-        setCredentials(prop, protocol);
-        try {
-            LsRemoteCommand lsRemote = new LsRemoteCommand(null);
-            lsRemote.setCredentialsProvider(credentialsProvider);
+	System.out.println("[*] Checking git credentials ...");
+	String protocol = getParameterString(prop, PARAMETER_GIT_PROTOCOL, true);
+	if (!SUPPORTED_PROTOCOLS.contains(protocol)) {
+	    System.out.println("[e] Unrecognized git protocol: " + protocol);
+	    System.out.println("[i] Supported protocols: " + String.join(",", SUPPORTED_PROTOCOLS));
+	    return false;
+	}
+	repository = getRepository(trimRight(getParameterString(prop, PARAMETER_GIT_REPOSITORY, false), "/"), protocol);
+	setCredentials(prop, protocol);
+	try {
+	    LsRemoteCommand lsRemote = new LsRemoteCommand(null);
+	    lsRemote.setCredentialsProvider(credentialsProvider);
 	    lsRemote.setTransportConfigCallback(transportConfigCallback);
-            lsRemote.setRemote(repository);
-            lsRemote.setHeads(true);
-            Collection<Ref> refs = lsRemote.call();
-            System.out.println("\t[i] " + refs.size() + " remote (head) refs found.");
-            System.out.println("\t[✓] Git credentials are ok.");
-            // https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/porcelain/ListRemotes.java
-            return true;
-        } catch (Exception ex) {
-            System.out.println("\t[e] Credentials failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
-        }
-        return false;
+	    lsRemote.setRemote(repository);
+	    lsRemote.setHeads(true);
+	    Collection<Ref> refs = lsRemote.call();
+	    System.out.println("\t[i] " + refs.size() + " remote (head) refs found.");
+	    System.out.println("\t[✓] Git credentials are ok.");
+	    // https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/porcelain/ListRemotes.java
+	    return true;
+	} catch (Exception ex) {
+	    System.out.println("\t[e] Credentials failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
+	}
+	return false;
     }
 
     private String getRepository(String repository, String protocol) {
-        int indexOfColonWithDoubleSlash;
-        if ((indexOfColonWithDoubleSlash = repository.indexOf("://")) > -1) {
-            repository = repository.substring(indexOfColonWithDoubleSlash + 3);
-        }
-        return protocol.equals("ssh") ? repository : protocol + "://" + repository;
+	int indexOfColonWithDoubleSlash;
+	if ((indexOfColonWithDoubleSlash = repository.indexOf("://")) > -1) {
+	    repository = repository.substring(indexOfColonWithDoubleSlash + 3);
+	}
+	return protocol.equals("ssh") ? repository : protocol + "://" + repository;
     }
 
     private void setCredentials(Properties prop, String protocol) {
@@ -157,8 +159,7 @@ public class BeginIssueWithGit extends Operation {
 	    if (!StringUtils.isBlank(username) && !StringUtils.isBlank(password)) {
 		credentialsProvider = new UsernamePasswordCredentialsProvider(username, password);
 	    }
-	}
-	else if (protocol.equals("ssh")) {
+	} else if (protocol.equals("ssh")) {
 	    final File identity = new File(getSshPrivateKey(prop));
 	    if (identity.exists()) {
 		final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory()
@@ -175,13 +176,9 @@ public class BeginIssueWithGit extends Operation {
 			return defaultJSch;
 		    }
 		};
-		transportConfigCallback = new TransportConfigCallback()
-		{
-		    @Override
-		    public void configure(Transport transport) {
-			SshTransport sshTransport = (SshTransport) transport;
-			sshTransport.setSshSessionFactory(sshSessionFactory);
-		    }
+		transportConfigCallback = (Transport transport) -> {
+		    SshTransport sshTransport = (SshTransport) transport;
+		    sshTransport.setSshSessionFactory(sshSessionFactory);
 		};
 	    }
 	}
@@ -193,33 +190,32 @@ public class BeginIssueWithGit extends Operation {
 	    if (sshPrivateKey.startsWith("~")) {
 		sshPrivateKey = System.getProperty("user.home") + sshPrivateKey.substring(1);
 	    }
-	}
-	else {
+	} else {
 	    sshPrivateKey = System.getProperty("user.home") + "/.ssh/id_rsa";
 	}
 	return sshPrivateKey;
     }
-    
+
     private boolean createBranch(Issue issue, Properties prop) {
-        System.out.println("[*] Creating branch ...");
-        String cacheDir = getWorkingDirectory() + "/" + "cache/git-data/repositories";
-        if (!createFolder(cacheDir)) {
+	System.out.println("[*] Creating branch ...");
+	String cacheDir = getWorkingDirectory() + "/" + "cache/git-data/repositories";
+	if (!createFolder(cacheDir)) {
 	    System.out.println("[e] Cache directory is unreachable! " + cacheDir);
 	    return false;
 	}
-        File cachePath = new File(cacheDir + "/" + 
-                new File(trimRight(getParameterString(prop, PARAMETER_GIT_REPOSITORY, false), "/")).getName());
+	File cachePath = new File(cacheDir + "/"
+		+ new File(trimRight(getParameterString(prop, PARAMETER_GIT_REPOSITORY, false), "/")).getName());
 	boolean isCacheReady;
-        if (!cachePath.exists()) {
-            isCacheReady = cloneRepository(cachePath);
-        } else {
-            isCacheReady = fetchRepository(cachePath);
-        }
+	if (!cachePath.exists()) {
+	    isCacheReady = cloneRepository(cachePath);
+	} else {
+	    isCacheReady = fetchRepository(cachePath);
+	}
 	if (isCacheReady) {
 	    listCurrentStatus(cachePath);
 	}
 
-        return true;
+	return true;
     }
 
     private void listCurrentStatus(File cachePath) {
@@ -228,23 +224,23 @@ public class BeginIssueWithGit extends Operation {
 	//listRepositoryLocalStatus(cachePath);
 	System.out.println("\t[✓] Done.");
     }
-    
+
     private boolean cloneRepository(File cachePath) {
-        System.out.println("\t[*] Locally cached repository not found. Cloning once ...");
-        try (Git result = Git.cloneRepository()
-                .setURI(repository)
-                .setCredentialsProvider(credentialsProvider)
-                .setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
-                .setDirectory(cachePath)
-                .call()) {
-            System.out.println("\t[✓] Cloned repository: " + result.getRepository().getDirectory());
-            return true;
-        } catch (GitAPIException gae) {
-            System.out.println("\t[e] Git clone failed with error! " + gae.getClass().getCanonicalName() + " " + gae.getMessage());
-        }
-        return false;
+	System.out.println("\t[*] Locally cached repository not found. Cloning once ...");
+	try (Git result = Git.cloneRepository()
+		.setURI(repository)
+		.setCredentialsProvider(credentialsProvider)
+		.setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
+		.setDirectory(cachePath)
+		.call()) {
+	    System.out.println("\t[✓] Cloned repository: " + result.getRepository().getDirectory());
+	    return true;
+	} catch (GitAPIException gae) {
+	    System.out.println("\t[e] Git clone failed with error! " + gae.getClass().getCanonicalName() + " " + gae.getMessage());
+	}
+	return false;
     }
-    
+
     private boolean fetchRepository(File cachePath) {
 	System.out.println("\t[*] Locally cached repository exists. Updating remote refs ...");
 	try (Repository gitRepo = openRepository(cachePath.getAbsolutePath())) {
@@ -261,17 +257,15 @@ public class BeginIssueWithGit extends Operation {
 		}
 		System.out.println("\t[✓] Git fetch completed.");
 		return true;
-	    }
-	    catch (GitAPIException ex) {
+	    } catch (GitAPIException ex) {
 		System.out.println("\t[e] Git fetch failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
 	    }
-	}
-	catch (IOException ex) {
+	} catch (IOException ex) {
 	    System.out.println("\t[e] Git fetch failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
 	}
 	return false;
     }
-    
+
     private Repository openRepository(String cachePath) throws IOException {
 	FileRepositoryBuilder builder = new FileRepositoryBuilder();
 	builder.setGitDir(new File(cachePath + "/.git"));
@@ -289,16 +283,16 @@ public class BeginIssueWithGit extends Operation {
 		List<Ref> refs = branchList.call();
 		if (refs.size() > 0) {
 		    System.out.println("\t[i] Branch list: ");
-		    for (Ref ref : refs) {
-			System.out.println("\t\t" + ref.getName() + " " + ref.getObjectId().getName());
-		    }
+		    refs.forEach((ref) -> {
+			System.out.print("\t\t" + ref.getName());
+			Optional.ofNullable(ref.getObjectId()).ifPresent(objectId -> System.out.print(" " + objectId.getName()));
+			System.out.println();
+		    });
 		}
-	    }
-	    catch (GitAPIException ex) {
+	    } catch (GitAPIException ex) {
 		System.out.println("\t[e] Git listing local branches failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
 	    }
-	}
-	catch (IOException ex) {
+	} catch (IOException ex) {
 	    System.out.println("\t[e] Git listing local branches failed! " + ex.getClass().getCanonicalName() + " " + ex.getMessage());
 	}
     }
