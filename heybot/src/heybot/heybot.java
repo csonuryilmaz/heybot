be92bc58317ad4b467085c6ccebc0f543dbf59ab
csonuryilmaz <csonuryilmaz@gmail.com>
2019-04-23 17:11:55 +0300

    Project is now portable between Netbeans and IntelliJ Idea.

--- heybot/src/heybot/heybot.java
+++ heybot/src/heybot/heybot.java
@@ -25,7 +25,7 @@ import utilities.Open;
 public class heybot
 {
 
-    private final static String VERSION = "2.5.1.4";
+    private final static String VERSION = "2.6.2.5";
     private static final String NEWLINE = System.getProperty("line.separator");
     public static final String WORKSPACE = System.getProperty("user.home") + "/.heybot/workspace";
 
