8190a56de67df0746b6ba17be34c0a65691c43b9
csonuryilmaz <csonuryilmaz@gmail.com>
2019-05-25 13:05:49 +0300

    Cleanup operation is refactored. #119

--- heybot/src/heybot/heybot.java
+++ heybot/src/heybot/heybot.java
@@ -25,7 +25,7 @@ import utilities.Open;
 public class heybot
 {
 
-    private final static String VERSION = "2.7.2.6";
+    private final static String VERSION = "2.7.2.7";
     private static final String NEWLINE = System.getProperty("line.separator");
     public static final String WORKSPACE = System.getProperty("user.home") + "/.heybot/workspace";
 
--- heybot/src/operation/Cleanup.java
+++ heybot/src/operation/Cleanup.java
@@ -1,15 +1,16 @@
 package operation;
 
-import com.taskadapter.redmineapi.RedmineException;
 import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.RedmineManagerFactory;
 import com.taskadapter.redmineapi.bean.Issue;
+import org.apache.commons.io.FileUtils;
+import utilities.Properties;
+
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Locale;
-import utilities.Properties;
 
 /**
  * Operation: cleanup
@@ -19,183 +20,130 @@ import utilities.Properties;
 public class Cleanup extends Operation
 {
 
-    private RedmineManager redmineManager;
     private final Locale trLocale = new Locale("tr-TR");
 
     //<editor-fold defaultstate="collapsed" desc="parameters">
     //mandatory
-    private final static String PARAMETER_LOCAL_PATH = "LOCAL_PATH";
-    private final static String PARAMETER_STATUS = "STATUS";
+    private final static String PARAMETER_WORKSPACE_PATH = "WORKSPACE_PATH";
+    private final static String PARAMETER_REDMINE_STATUS = "REDMINE_STATUS";
     private final static String PARAMETER_REDMINE_TOKEN = "REDMINE_TOKEN";
     private final static String PARAMETER_REDMINE_URL = "REDMINE_URL";
     // optional
     private final static String PARAMETER_LIMIT = "LIMIT";
-    private final static String PARAMETER_IS_DELETE_ENABLED_WHEN_NOT_FOUND = "IS_DELETE_ENABLED_WHEN_NOT_FOUND";
+    private final static String PARAMETER_DELETE_WHEN_ISSUE_NOT_FOUND = "DELETE_WHEN_ISSUE_NOT_FOUND";
 
-//</editor-fold>
-    public Cleanup()
-    {
-	super(new String[]
-	{
-	    PARAMETER_LOCAL_PATH, PARAMETER_STATUS, PARAMETER_REDMINE_TOKEN, PARAMETER_REDMINE_URL
-	}
-	);
+    //</editor-fold>
+    public Cleanup() {
+        super(new String[]
+            {
+                PARAMETER_WORKSPACE_PATH, PARAMETER_REDMINE_STATUS, PARAMETER_REDMINE_TOKEN, PARAMETER_REDMINE_URL
+            }
+        );
     }
 
     @Override
-    public void execute(Properties prop)
-    {
-	if (areMandatoryParametersNotEmpty(prop))
-	{
-	    // try get parameters
-	    String localWorkingDirectory = getParameterString(prop, PARAMETER_LOCAL_PATH, false);
-	    HashSet<String> statuses = getParameterStringHash(prop, PARAMETER_STATUS, true);
-	    String redmineAccessToken = getParameterString(prop, PARAMETER_REDMINE_TOKEN, false);
-	    String redmineUrl = getParameterString(prop, PARAMETER_REDMINE_URL, false);
-
-	    int max = getParameterInt(prop, PARAMETER_LIMIT, Integer.MAX_VALUE);
-
-	    start(localWorkingDirectory, redmineAccessToken, redmineUrl, max, statuses, prop);
-	}
+    public void execute(Properties prop) {
+        if (areMandatoryParametersNotEmpty(prop)) {
+            cleanWorkspace(prop);
+        }
     }
 
-    private void start(String localWorkingDirectory, String redmineAccessToken, String redmineUrl, int max, HashSet<String> statuses, Properties prop)
-    {
-	boolean isDeleteEnabledWhenNotFound = getParameterBoolean(prop, PARAMETER_IS_DELETE_ENABLED_WHEN_NOT_FOUND);
-	redmineManager = RedmineManagerFactory.createWithApiKey(redmineUrl, redmineAccessToken);
+    private void cleanWorkspace(Properties prop) {
+        String workspacePath = getParameterString(prop, PARAMETER_WORKSPACE_PATH, false);
+        if (!workspacePath.endsWith("/")) {
+            workspacePath += "/";
+        }
+        HashSet<String> statuses = getParameterStringHash(prop, PARAMETER_REDMINE_STATUS, true);
+        String redmineAccessToken = getParameterString(prop, PARAMETER_REDMINE_TOKEN, false);
+        String redmineUrl = getParameterString(prop, PARAMETER_REDMINE_URL, false);
 
-	Branch[] branches = getBranches(localWorkingDirectory);
-	for (Branch branch : branches)
-	{
-	    Issue issue = getIssue(redmineManager, branch.getIssueId());
-	    if (issue == null)
-	    {
-		System.out.println("#" + branch.getIssueId() + " Not found!");
-		if (isDeleteEnabledWhenNotFound)
-		{
-		    deleteBranch(localWorkingDirectory, branch);
-		}
-	    }
-	    else
-	    {
-		System.out.println("#" + issue.getId() + " - " + issue.getSubject());
-		if (statuses.contains(issue.getStatusName().toLowerCase(trLocale)))
-		{
-		    deleteBranch(localWorkingDirectory, branch);
-		}
-	    }
+        boolean isDeleteEnabledWhenNotFound = getParameterBoolean(prop, PARAMETER_DELETE_WHEN_ISSUE_NOT_FOUND);
+        int max = getParameterInt(prop, PARAMETER_LIMIT, Integer.MAX_VALUE);
 
-	    if (--max <= 0)
-	    {
-		break; // limit is reached!
-	    }
-	}
+        RedmineManager redmineManager = RedmineManagerFactory.createWithApiKey(redmineUrl, redmineAccessToken);
+
+        Branch[] branches = getBranches(workspacePath);
+        for (Branch branch : branches) {
+            Issue issue = getIssue(redmineManager, branch.getIssueId());
+            if (issue == null) {
+                System.out.println("[w] #" + branch.getIssueId() + " not found on Redmine!");
+                if (isDeleteEnabledWhenNotFound) {
+                    deleteBranch(workspacePath, branch);
+                    max--;
+                }
+            } else {
+                System.out.println("[i] #" + issue.getId() + " - " + issue.getSubject());
+                System.out.println("\t |_> " + branch.getDirectory() + "/");
+                System.out.println("\t |_> " + issue.getStatusName());
+                if (statuses.contains(issue.getStatusName().toLowerCase(trLocale))) {
+                    deleteBranch(workspacePath, branch);
+                    max--;
+                }
+            }
+
+            if (max == 0) {
+                break; // limit is reached!
+            }
+        }
     }
 
-    private Branch[] getBranches(String dir)
-    {
-	String[] directories = new File(dir).list(new FilenameFilter()
-	{
-	    @Override
-	    public boolean accept(File current, String name)
-	    {
-		return new File(current, name).isDirectory();
-	    }
-	});
-
-	ArrayList<Branch> branches = new ArrayList<>();
-
-	if (directories != null)
-	{
-	    Branch branch;
-	    for (String directory : directories)
-	    {
-		if ((branch = tryGetBranch(directory)) != null)
-		{
-		    branches.add(branch);
-		}
-	    }
-	}
-	return branches.toArray(new Branch[0]);
+    private Branch[] getBranches(String dir) {
+        String[] directories = new File(dir).list((current, name) -> new File(current, name).isDirectory());
+        ArrayList<Branch> branches = new ArrayList<>();
+        if (directories != null) {
+            Branch branch;
+            for (String directory : directories) {
+                if ((branch = tryGetBranch(directory)) != null) {
+                    branches.add(branch);
+                }
+            }
+        }
+        return branches.toArray(new Branch[0]);
     }
 
-    private Branch tryGetBranch(String directory)
-    {
-	String issueId = directory.replaceAll("[^0-9]", "");
-	if (issueId.length() > 0)
-	{
-	    try
-	    {
-		return new Branch(Integer.parseInt(issueId), directory);
-	    }
-	    catch (NumberFormatException nfe)
-	    {
-		// ignore, null will be returned
-	    }
-	}
+    private Branch tryGetBranch(String directory) {
+        String issueId = directory.replaceAll("[^0-9]", "");
+        if (issueId.length() > 0) {
+            try {
+                return new Branch(Integer.parseInt(issueId), directory);
+            } catch (NumberFormatException nfe) {
+                // ignore, null will be returned
+            }
+        }
 
-	return null;
+        return null;
     }
 
-    private String getIssueStatus(int issueId)
-    {
-	try
-	{
-	    Issue issue = redmineManager.getIssueManager().getIssueById(issueId);
-
-	    return issue.getStatusName().toLowerCase(trLocale);
-	}
-	catch (RedmineException ex)
-	{
-	    System.err.println(System.getProperty("line.separator") + "Ooops! Error while checking issue status " + issueId + ". (" + ex.getMessage() + ")");
-	}
-
-	return ""; // unknown!
-    }
-
-    private void deleteBranch(String sftpBranchLocDir, Branch branch)
-    {
-	String path;
-	if (sftpBranchLocDir.endsWith("/"))
-	{
-	    path = sftpBranchLocDir + branch.getDirectory();
-	}
-	else
-	{
-	    path = sftpBranchLocDir + "/" + branch.getDirectory();
-	}
-
-	System.out.print("Deleting from " + path + "  ...");
-	if (tryExecute(new String[]
-	{
-	    "rm", "-Rf", path
-	}))
-	{
-	    System.out.println(" [✓]");
-	}
+    private void deleteBranch(String workspacePath, Branch branch) {
+        File path = new File(workspacePath + branch.getDirectory());
+        System.out.println("[*] Deleting: " + path.getAbsolutePath());
+        try {
+            FileUtils.deleteDirectory(path);
+            System.out.println("[✓] Deleted.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("[e] " + e.getMessage());
+        }
     }
 
     class Branch
     {
 
-	private final int issueId;
-	private final String directory;
+        private final int issueId;
+        private final String directory;
 
-	Branch(int issueId, String directory)
-	{
-	    this.issueId = issueId;
-	    this.directory = directory;
-	}
+        Branch(int issueId, String directory) {
+            this.issueId = issueId;
+            this.directory = directory;
+        }
 
-	int getIssueId()
-	{
-	    return issueId;
-	}
+        int getIssueId() {
+            return issueId;
+        }
 
-	String getDirectory()
-	{
-	    return directory;
-	}
+        String getDirectory() {
+            return directory;
+        }
     }
 
 }
