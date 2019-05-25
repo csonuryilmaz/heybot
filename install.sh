f78108274c19059b08bc732efb0f7a2566efdb9b
csonuryilmaz <csonuryilmaz@gmail.com>
2019-05-25 16:07:34 +0300

    install.sh script is added. #120

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+LATEST="2.7.2.7"
+FOLDER="$HOME/Downloads"
+
+set -e;
+cd "$HOME"
+echo "[*] Downloading latest release into: $FOLDER"
+mkdir -p "$FOLDER" 
+wget --backups=1 https://github.com/csonuryilmaz/heybot/releases/download/$LATEST/heybot-$LATEST.tar.gz -P "$FOLDER"
+printf "[\xE2\x9C\x94] Downloaded. \n"
+echo "[*] Extracting archive with tar ..."
+tar -zxvf "$FOLDER/heybot-$LATEST.tar.gz"
+printf "[\xE2\x9C\x94] Extracted. \n"
+chmod a+x "heybot-$LATEST/install.sh"
+heybot-$LATEST/install.sh
