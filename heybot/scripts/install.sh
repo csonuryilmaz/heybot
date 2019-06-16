e891cd2c2cfef6e66a32dc25602ba4930bc4a96e
csonuryilmaz <csonuryilmaz@gmail.com>
2019-06-16 18:57:21 +0300

    WIP: #122 IntelliJ Idea inspection fixes.

--- heybot/scripts/install.sh
+++ heybot/scripts/install.sh
@@ -11,16 +11,16 @@ strIndex()
 removeOldHeybot()
 {
     echo "[*] Finding old heybot installation ..."
-    if [ -e "$HOME/.heybot/installed.path" ]; then
+    if [[ -e "$HOME/.heybot/installed.path" ]]; then
         HEYBOT_INSTALLED_PATH=$(head -1 $HOME/.heybot/installed.path)
-        echo "[i] "$HEYBOT_INSTALLED_PATH
+        echo "[i] "${HEYBOT_INSTALLED_PATH}
         read -p "[?] Removing old heybot installation. Are you sure? (y/n) " -n 1 -r
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 	    echo ""
 	    echo "[*] Removing old heybot installation ..."
 	    sudo rm -f /usr/local/bin/heybot
-	    sudo rm -Rf $HEYBOT_INSTALLED_PATH
-	    if [ -d "$HEYBOT_INSTALLED_PATH" ]; then
+	    sudo rm -Rf ${HEYBOT_INSTALLED_PATH}
+	    if [[ -d "$HEYBOT_INSTALLED_PATH" ]]; then
 		echo "[w] Could not be removed!"
 	    else
 		printf "[\xE2\x9C\x94] Removed.\n"
@@ -47,28 +47,28 @@ install()
 {
     echo "[*] Installing ... "
     setDirectory
-    sudo ln -s $DIRECTORY/heybot.run /usr/local/bin/heybot && sudo chmod +x /usr/local/bin/heybot
+    sudo ln -s ${DIRECTORY}/heybot.run /usr/local/bin/heybot && sudo chmod +x /usr/local/bin/heybot
     LN_RESULT="$?"
-    if [ "$LN_RESULT" -ne 0 ]; then
+    if [[ "$LN_RESULT" -ne 0 ]]; then
         echo "[e] Could not create runnable in /usr/local/bin!"
         exit 1
     fi
-    if [ -x "$(command -v heybot)" ]; then
+    if [[ -x "$(command -v heybot)" ]]; then
         HEYBOT_LINK=$(command -v heybot)
-        echo -e "\t[i] "$HEYBOT_LINK
-        HEYBOT_EXEC=$(ls -lh $HEYBOT_LINK | awk '{print $(NF-1), $NF}' | tail -1)
-        echo -e "\t[i] "$HEYBOT_EXEC
+        echo -e "\t[i] "${HEYBOT_LINK}
+        HEYBOT_EXEC=$(ls -lh ${HEYBOT_LINK} | awk '{print $(NF-1), $NF}' | tail -1)
+        echo -e "\t[i] "${HEYBOT_EXEC}
         FIRST_INDEX_OF_SLASH=$(strIndex "${HEYBOT_EXEC}" "/")
         HEYBOT_RUN="${HEYBOT_EXEC##*/}"
         LAST_INDEX_OF_SLASH=$(strIndex "${HEYBOT_EXEC}" "/${HEYBOT_RUN}")
-        HEYBOT_FOLDER=$(echo "${HEYBOT_EXEC}" | cut -c $FIRST_INDEX_OF_SLASH-$LAST_INDEX_OF_SLASH)
+        HEYBOT_FOLDER=$(echo "${HEYBOT_EXEC}" | cut -c ${FIRST_INDEX_OF_SLASH}-${LAST_INDEX_OF_SLASH})
         HEYBOT_FOLDER="$(echo -e "${HEYBOT_FOLDER}" | sed -e 's/^[[:space:]]*//')"
-        echo -e "\t[i] "$HEYBOT_FOLDER
-        if [ ! -d "$HOME/.heybot" ]; then
+        echo -e "\t[i] "${HEYBOT_FOLDER}
+        if [[ ! -d "$HOME/.heybot" ]]; then
         mkdir $HOME/.heybot
         echo -e "\t[i] Created "$HOME/.heybot
         fi
-        echo $HEYBOT_FOLDER > $HOME/.heybot/installed.path
+        echo ${HEYBOT_FOLDER} > $HOME/.heybot/installed.path
         echo -e "\t[i] Installation folder is saved."
         printf "[\xE2\x9C\x94] Installed! \\o/ \n"
         exit 0
@@ -91,9 +91,9 @@ fi
 
 BIN_FOLDER="/usr/local/bin"
 echo "[*] Checking is $BIN_FOLDER exists? ..."
-if [ ! -d "$BIN_FOLDER" ]; then
+if [[ ! -d "$BIN_FOLDER" ]]; then
     echo "Not exists! Needs to create directory ..."
-    sudo mkdir $BIN_FOLDER && printf "\n[\xE2\x9C\x94]\n"
+    sudo mkdir ${BIN_FOLDER} && printf "\n[\xE2\x9C\x94]\n"
 else
     printf "[\xE2\x9C\x94] Exists.\n"
 fi
